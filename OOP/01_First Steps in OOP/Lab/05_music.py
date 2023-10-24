9dc23745b85af4e53050a27dcfd4e3bdd518531e
stefipop <132662101+stefipop@users.noreply.github.com>
2023-10-24 20:18:07 +0300

    lab

---
+++ OOP/01_First Steps in OOP/Lab/05_music.py
@@ -0,0 +1,14 @@
+class Music():
+    def __init__(self, title: str, artist: str, lyrics: str):
+        self.title = title
+        self.artist = artist
+        self.lyrics = lyrics
+
+    def print_info(self) -> str:
+        return f'This is "{self.title}" from "{self.artist}"'
+
+    def play(self):
+        return self.lyrics
+
+
+song1 = Music("Godzilla", "Eminem", "Blablabla")
